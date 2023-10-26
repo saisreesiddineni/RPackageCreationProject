83318b27061eb9208b8fc2c0cc9f471ce6b381f0
Saisree <saisree.siddineni@5datainc.com>
2023-10-26 16:26:01 -0700

    Gmm Algo files

---
+++ src/interface.cpp
@@ -0,0 +1,24 @@
+#include "gmm.h"
+#include <R_ext/Rdynload.h>
+
+void fitGMM_interface(
+    std::vector<double>& data, 
+    int *numComponents, 
+    int *maxIters)
+{
+  fitGMM(data, *numComponents, *maxIters);
+}
+
+
+R_CMethodDef cMethods[] = {
+  {"fitGMM_interface", (DL_FUNC) &fitGMM_interface, 3},
+  {NULL,NULL,0}
+};
+
+extern "C"{
+  void R_init_RPackageCreationProject(DllInfo *info)
+  {
+    R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
+  }
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(RPackageCreationProject)
+
+test_check("RPackageCreationProject")
